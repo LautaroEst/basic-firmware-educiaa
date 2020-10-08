070ac7293ca6248b5f581105fe5ff17ccea8e43c
FedericoMunoz <fmunozm@fi.uba.ar>
2020-10-08 20:31:04 -0300

    Estructura Stack_t

---
+++ practica-git/inc/stack.h
@@ -0,0 +1,13 @@
+/*
+ * stack.h
+ *
+ *  Created on: 8 oct. 2020
+ *      Author: Federico
+ */
+
+#ifndef INC_STACK_H_
+#define INC_STACK_H_
+
+
+
+#endif /* INC_STACK_H_ */
--- practica-git/src/main.c
+++ practica-git/src/main.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_BUFFER 500
 
-int main(){
+int main(int argc, char const *argv[]){
+	char buffer[MAX_BUFFER];
+	bool reading = true;
 
+	while(reading){
+		fgets(buffer,MAX_BUFFER,stdin);
+		if(strcmp())
+	}
 	return 0;
 }
