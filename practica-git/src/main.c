d66df9e334c2aadec4ef4b4cf1eafbf3b098b559
LeaPepe <leapalazzo@gmail.com>
2020-10-09 12:20:06 -0300

    estructura de la funcion main y los archivos para el stack con las estructuras basicas

--- practica-git/src/main.c
+++ practica-git/src/main.c
@@ -1,12 +1,42 @@
 #include "main.h"
 
-// programas
+#DEFINE LABEL_SUM = "suma"
+#DEFINE LABEL_PROD = "prod"
 
-float suma(float var1,float var2){
-	return var1 + var2;
-}
-
-
+// ------------ //
+// --- Main --- //
+// ------------ //
 int main(void){
+	
+	// variables used
+	char* input; // input string
+	stack_t data; // numbers stack
+	double number; // number parsed
+	double ans; // answer
+	bool isProd;
+	
+	// collect numbers
+	fgets(input); // get first input
+	while(input != LABEL_SUM && input != LABEL_PROD){ // while not sum or prod
+		//validate and translate to double
+		data.push(number)
+		fgets(input);
+	}
+	
+	// check type of function (prod or sum)
+	if(input == LABEL_PROD){
+		isProd = true;
+	} else {
+		isProd = false;
+	}
+	
+	// do the math
+	if(isProd){
+		// mult function
+	} else {
+		// sum function
+	}
+	
+	// print answer
 	return 0;
 }
