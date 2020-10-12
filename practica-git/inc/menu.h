c8ac8b1361bcbfb785b9f50bf14244f60983612b
andres <72326236+sequeirandres@users.noreply.github.com>
2020-10-12 18:54:36 -0300

    add messages, pila, menu, setting, validation

---
+++ practica-git/inc/menu.h
@@ -0,0 +1,8 @@
+
+#ifndef __MENU__
+#define __MENU__
+
+#define MENU_OPTION_SUM "sum"
+#define MENU_OPTION_PRODUCT "prod"
+
+#endif
---
+++ practica-git/inc/setting.h
@@ -0,0 +1,11 @@
+
+
+#ifndef __SETTING__
+#define __SETTING__
+
+#define MAX_SIZE_STRING 10
+#define MAX_NODE 10
+
+#endif
+
+
